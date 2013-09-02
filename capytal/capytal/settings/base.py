8f6bfe0e74fa2372c81b9d8c095a0b516de523fe
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-09-02 21:35:45 +0200

    Definition of base root dirs
    Definition of the following directories:
     * static
     * media
     * assets
     * templates
    
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- capytal/capytal/settings/base.py
+++ capytal/capytal/settings/base.py
@@ -1,6 +1,7 @@
 # Django settings for capytal project.
 
 import os
+from unipath import Path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -22,6 +23,8 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+
+PROJECT_DIR = Path(__file__).ancestor(3)
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -52,7 +55,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PROJECT_DIR.child('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -65,7 +68,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR.child('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -73,9 +76,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_DIR.child('assets'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,9 +110,7 @@ ROOT_URLCONF = 'capytal.urls'
 WSGI_APPLICATION = 'capytal.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_DIR.child('templates')
 )
 
 INSTALLED_APPS = (
