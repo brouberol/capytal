2c9be860e4e8977826748249ddeefee3a450c4d0
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-09-06 17:02:50 +0200

    Tweaking of the Expense admin interface
    Display of the fields:
     * name
     * owner
     * amount
     * date
    
    The expenses can be filtered by:
     * date
     * owner
    
    The name is used for the search field
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- capytal/expense/admin.py
+++ capytal/expense/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Expense
 
-admin.site.register(Expense)
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'amount', 'date')
+    list_filter = ('owner', 'date')
+    search_fields = ('name',)
+
+admin.site.register(Expense, ExpenseAdmin)
