4a5a475c13c98a35997eb4eb541c56ff41ad33c1
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-09-06 17:02:39 +0200

    Integration of the Category model into Expense
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- capytal/expense/models.py
+++ capytal/expense/models.py
@@ -4,16 +4,25 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator
 
+from category.models import Category
+
 
 class Expense(models.Model):
     """The Expense class holds information about who spend what and when"""
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-        related_name='+', verbose_name=u'émetteur')
-    amount = models.FloatField(validators=[MinValueValidator(0)],
-        verbose_name=u'montant')
-    recipients = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        related_name='+', verbose_name=u'bénéficiaires')
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='+', verbose_name=u'émetteur')
+    amount = models.FloatField(
+        validators=[MinValueValidator(0)], verbose_name=u'montant')
+    recipients = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='+',
+        verbose_name=u'bénéficiaires')
     name = models.CharField(max_length=100, verbose_name=u'nom')
+    date = models.DateTimeField()
+    category =  models.ForeignKey(Category, related_name='+')
 
     def __unicode__(self):
         return u'%s - %d€' % (self.name, self.amount)
+
+    class Meta:
+        verbose_name = u'dépense'
+        verbose_name_plural = u'dépenses'
