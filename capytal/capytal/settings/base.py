cf23ac8fe8970b3d62a4287a9f488d710901fdc9
Balthazar Rouberol <brouberol@imap.cc>
2013-09-12 23:55:52 +0200

    Fix 404 HTTP errors on all static files Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/capytal/settings/base.py
+++ capytal/capytal/settings/base.py
@@ -68,7 +68,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = PROJECT_DIR.child('static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -76,6 +76,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),
     PROJECT_DIR.child('assets'),
 )
 
