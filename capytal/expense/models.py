875e336360273e3e77cdac3d4494d8c7e5ae9c0b
Balthazar Rouberol <brouberol@imap.cc>
2013-09-13 02:16:31 +0200

    Update owner balance after Expense save/update
    If the Expense is new, its owner balance will be increased by
    its amount. However, if the Expense has simply been updated
    and the price has changed, the change of price is reflected into
    the owner balance too.
    Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/expense/models.py
+++ capytal/expense/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator
+from django.db.models.signals import pre_save, pre_delete
 
 from category.models import Category
 from roommate.models import Roommate
@@ -27,3 +28,32 @@ class Expense(models.Model):
     class Meta:
         verbose_name = u'dépense'
         verbose_name_plural = u'dépenses'
+
+
+def increment_owner_balance(sender, instance, raw, using, **kwargs):
+    """Increment the expense owner balance before the expense is saved
+    for the first time or when the amount has been updated.
+
+    """
+    owner = instance.owner
+    if instance.id:
+        # Existing Expense has been modified
+        old_expense = Expense.objects.get(id=instance.id)
+        if old_expense.amount != instance.amount:
+            owner.balance += (instance.amount - old_expense.amount)
+            owner.save()
+    else:
+        # New Expense is saved to DB
+        owner.balance += instance.amount
+        owner.save()
+
+
+def decrement_owner_balance(sender, instance, using, **kwargs):
+    """Decrement the expense owner balance before the expense is deleted."""
+    owner = instance.owner
+    owner.balance -= instance.amount
+    owner.save()
+
+
+pre_save.connect(increment_owner_balance, sender=Expense)
+pre_delete.connect(decrement_owner_balance, sender=Expense)
