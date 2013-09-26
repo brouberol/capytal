c4f4be4c9772d1cc0c74b5aefc39e12ccc0b3091
Balthazar Rouberol <brouberol@imap.cc>
2013-09-26 18:50:32 +0200

    Improve docstring
    Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/expense/utils.py
+++ capytal/expense/utils.py
@@ -7,7 +7,7 @@ from __future__ import division
 
 def balance_share(expense, recipient, nb_recipients):
     """Return the expense share of the recipient given the number of
-    recipients concerned by it.
+    recipients it concerns.
 
     If the recipient is the owner of the expense, the result will be positive,
     else, it will be negative.
