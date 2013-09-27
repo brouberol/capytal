b943ba3946e0ded4c9e5e9e8f87dc540ea4f8162
Balthazar Rouberol <brouberol@imap.cc>
2013-09-27 19:55:48 +0200

    Pep8
    Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/roommate/urls.py
+++ capytal/roommate/urls.py
@@ -10,9 +10,12 @@ from .views import RoommateUpdateView
 from .views import RoommateDetailView
 
 
-urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)$', RoommateDetailView.as_view(), name="roommate_display"),
+urlpatterns = patterns(
+    '',
+    url(r'^(?P<pk>\d+)$', RoommateDetailView.as_view(),
+        name="roommate_display"),
     url(r'^$', RoommateListView.as_view(), name="roommate_list"),
     url(r'^new$', RoommateCreateView.as_view(), name="roommate_create"),
-    url(r'^(?P<pk>\d+)/update$', RoommateUpdateView.as_view(), name="roommate_update"),
+    url(r'^(?P<pk>\d+)/update$',
+        RoommateUpdateView.as_view(), name="roommate_update"),
 )
