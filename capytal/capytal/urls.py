dc6e1b439e1351e390e16ef5250cfb76122d255f
Balthazar Rouberol <brouberol@imap.cc>
2013-09-27 19:53:44 +0200

    The url /<category-slug> leads to a summary of all expenses in that category
    The Roommate is the one linked to the request user.
    The expenses are ordered by descendant date.
    Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/capytal/urls.py
+++ capytal/capytal/urls.py
@@ -1,18 +1,25 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from .views import expense_summary
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'capytal.views.home', name='home'),
     # url(r'^capytal/', include('capytal.foo.urls')),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include(
+        'django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'capytal.views.homepage', name='homepage'),
     url(r'^login/', 'capytal.views.user_login', name='user_login'),
-    url(r'^logout/', 'capytal.views.user_logout', name='user_logout'),
+    url(r'^logout/', 'capytal.views.user_logout',
+        name='user_logout'),
     url(r'^expense/', include('expense.urls')),
     url(r'^roommate/', include('roommate.urls')),
+    url(r'^(?P<cat_slug>[\w-]+)',
+        expense_summary, name="roommate_expenses_by_category")
 )
--- capytal/capytal/views.py
+++ capytal/capytal/views.py
@@ -3,8 +3,12 @@ General application wide views.
 """
 
 from django.contrib.auth.views import login, logout
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
+
+from category.models import Category
+from roommate.models import Roommate
+from expense.models import Expense
 
 
 def homepage(request, *args, **kwargs):
@@ -21,3 +25,18 @@ def user_logout(request, *args, **kwargs):
     """Log the user out and redirect him/her to the homepage"""
     logout(request, *args, **kwargs)
     return redirect('capytal.views.homepage')
+
+
+def expense_summary(request, cat_slug):
+    """Display the summary of the request roommate expenses for the given category"""
+    roommate = get_object_or_404(Roommate, user=request.user)
+    category = get_object_or_404(Category, slug=cat_slug)
+    expense_list = Expense.objects.filter(category=category).order_by('-date')
+    return render_to_response(
+        'roommate/roommate_expense_category.html',
+        {
+            'roommate': roommate,
+            'category': category,
+            'expense_list': expense_list,
+        },
+        RequestContext(request))
