005ec88c611647e99735eb37dac3baa7d6f40809
Balthazar Rouberol <brouberol@imap.cc>
2013-09-27 19:28:35 +0200

    Roommate slug automatically generated from associated username Signed-off-by: Balthazar Rouberol <brouberol@imap.cc>

--- capytal/roommate/models.py
+++ capytal/roommate/models.py
@@ -8,19 +8,26 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.template.defaultfilters import slugify
 
 
 class Roommate(models.Model):
     balance = models.FloatField(default=0.0, verbose_name=u'solde')
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=30, unique=True)
 
     def __unicode__(self):
-        return self.user.username
+        return self.slug
 
     class Meta:
         verbose_name = 'colocataire'
         verbose_name_plural = 'colocataires'
+
+    def save(self, *args, **kwargs):
+        """Generate the Roommate slug from its username before saving the model."""
+        self.slug = slugify(self.user.username)
+        super(Roommate, self).save(*args, **kwargs)
 
 
 def create_profile(sender, instance, created, **kwargs):
--- capytal/roommate/tests.py
+++ capytal/roommate/tests.py
@@ -6,11 +6,23 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import Roommate
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class RoommateTest(TestCase):
+
+    def test_generate_slug_at_save(self):
+        """Checks that the Roommate slug is generated when saved."""
+        user = User.objects.create(username='test1')
+        user.save()
+        roommate = Roommate.objects.get(user=user)
+        self.assertEqual(roommate.slug, 'test1')
+
+    def test_generate_slug_at_save_with_complicated_username(self):
+        """Checks that the Roommate slug is generated when saved."""
+        user = User.objects.create(username='stormagan the bold')
+        user.save()
+        roommate = Roommate.objects.get(user=user)
+        self.assertEqual(roommate.slug, 'stormagan-the-bold')
